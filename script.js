b515351451be7b587dcf1debbe04539c38882c6e
Max Lemieux <max.lemieux@gmail.com>
2020-02-02 14:23:21 -0800

    Added password option dialog functions

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getPwLength() {
+  var lengthIsSet = false;
+  while (lengthIsSet === false) {
+    var pwLength = prompt("Enter length of desired password (8-128 characters)");
+    pwLength = parseInt(pwLength);
+    if (typeof pwLength === "number" && 8 <= pwLength && pwLength <= 128) {
+      lengthIsSet = true;
+    } else {
+      alert("Please enter a number between 8 and 128.");
+    }
+  }
+  console.log(pwLength);
+  return pwLength;
+}
+function generatePassword() {
+  var pwLength = getPwLength();
+  var pwLowercase = confirm("Use lowercase letters in the password?");
+  var pwUppercase = confirm("Use uppercase letters in the password?");
+  var pwNumbers = confirm("Use numbers in the password?");
+  var pwSpecialChar = confirm("Use special characters in the password?");
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
