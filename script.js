58003a725c29cda0587fb79c87c5f1784795cefc
Max Lemieux <max.lemieux@gmail.com>
2020-02-02 15:19:18 -0800

    Added missing semicolons

--- script.js
+++ script.js
@@ -19,12 +19,12 @@ function getPwLength() {
 
 // Get all the information from the user and define the possible sets of characters
 function generatePassword() {
-  var password = ''
-  var possibleChars = []
-  var lowerChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-  var upperChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  var numberChars = ['1','2','3','4','5','6','7','8','9','0']
-  var specialChars = ['!','@','#','$','%','^','&','*','(',')','-','=','_','+']
+  var password = '';
+  var possibleChars = [];
+  var lowerChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var upperChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var numberChars = ['1','2','3','4','5','6','7','8','9','0'];
+  var specialChars = ['!','@','#','$','%','^','&','*','(',')','-','=','_','+'];
   
   // Array of all possible parts of the password
   var charsArr = [lowerChars, upperChars, numberChars, specialChars];
